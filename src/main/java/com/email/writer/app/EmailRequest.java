1569ed6c891794389fafed2155c92acb3a4acb55
Shital Bhosale <shitalb075@gmail.com>
2026-03-26 18:05:21 +0530

    added code

---
+++ src/main/java/com/email/writer/app/EmailRequest.java
@@ -0,0 +1,17 @@
+package com.email.writer.app;
+
+import lombok.Data;
+
+@Data
+
+public class EmailRequest {
+    private String emailContent;
+    private String tone;
+
+    public CharSequence getTone() {
+    }
+
+    public char[] getEmailContent() {
+        return null;
+    }
+}
